a8b7bcd9fbbde31dfa6fa8456b0d925cbf6691ad
hanjx12024 <hanjx12024@shanghaitech.edu.cn>
2026-03-16 12:50:51 +0800

    task2 and improvement fot task1

--- queue.c
+++ queue.c
@@ -5,13 +5,17 @@
 Queue *queue_create(void)
 {
   Queue *queue = malloc(sizeof(Queue));
-  if(!queue) return NULL;
+  if(!queue){
+    printf("create fail");
+    return NULL;
+  }
 
   queue->size = 0;
   queue->capacity = QUEUE_INITIAL_CAPACITY;
   double* temp = malloc(sizeof(double) * queue->capacity);
   if (!temp) {
     free(queue);
+    printf("create fail");
     return NULL;
   }
   queue->data = temp;
@@ -20,12 +24,19 @@ Queue *queue_create(void)
 
 void push(Queue *queue, double element)
 {
+  if(!queue) {
+    printf("Invalid queue");
+    return;
+  }
   if (queue->size == queue->capacity)
   {
     int capacity = queue->capacity * 2;
 
     double* temp = realloc(queue->data, sizeof(double) * capacity);
-    if(!temp)return;
+    if(!temp){
+      printf("create fail");
+      return;
+    }
     queue->data = temp;
 
     queue->capacity = capacity;
@@ -38,13 +49,17 @@ void push(Queue *queue, double element)
 
 double back(Queue *queue)
 {
+  if(!queue) {
+    printf("invalid queue");
+    return 0.0;
+  }
   return queue->data[queue->size - 1];
 }
 
 void queue_free(Queue *queue)
 {
-
-  free(queue->data);
+  if(!queue) return;
+  if(queue -> data)free(queue->data);
   free(queue);
 }
 
